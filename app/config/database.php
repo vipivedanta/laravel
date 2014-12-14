e6148867f8092c4b5c1eaaf9f9e503b85ecbada4
Vipin Kumar <vipink@admin-ahead.com>
2014-12-14 11:47:00 +0530

    added database details

--- app/config/database.php
+++ app/config/database.php
@@ -55,8 +55,8 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
+			'database'  => 'larave;',
+			'username'  => 'vipin',
 			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
