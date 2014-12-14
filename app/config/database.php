4d7c8a698f0d476921c746ddcf097c8e25077c1e
Vipin Kumar <vipink@admin-ahead.com>
2014-12-14 15:28:33 +0530

    Updated database file error

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'larave;',
+			'database'  => 'laravel',
 			'username'  => 'vipin',
 			'password'  => '',
 			'charset'   => 'utf8',
